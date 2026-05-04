33eaa556ae93bbd00e7b588842efd25bedf92e51
truongkatelyn6 <truongkatelyn6@csu.fullerton.edu>
2026-05-04 16:43:18 -0700

    ground

--- assets/main.py
+++ assets/main.py
@@ -98,7 +98,19 @@ class Fence(pygame.sprite.Sprite):
                 self.passed = True
                 score += 1
 # ground class
+class Ground(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = ground_image
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = x, y
 
+    def update(self):
+        # Moving ground
+        self.rect.x -= scroll_speed
+        if self.rect.x <= -SCREEN_WIDTH:
+            self.kill()
+            
 # exiting game
 def quit_game():
   for event in pygame.event.get():
